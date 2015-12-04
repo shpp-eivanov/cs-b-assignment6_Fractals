a5c59859fe51a010ad6be6ea2e386c61b30d2cc2
ekuryatenko <ekuryatenko@gmail.com>
2015-12-04 09:11:30 +0200

    FINAL COMMIT

--- Mandelbrot/src/Mandelbrot.cpp
+++ Mandelbrot/src/Mandelbrot.cpp
@@ -1,49 +1,151 @@
-﻿#include <iostream>
+﻿/********************************************************************************************
+ * File: Mandelbrot.cpp
+ * ----------------------
+ * v.1 2015/11/30
+ * Implementation of the Mandelbrot set drawing.
+ ********************************************************************************************/
+
+#include <iostream>
 #include "console.h"
 #include <stdlib.h>
-#include "queue.h"
-#include <Vector>
 #include "gbufferedimage.h"
-
 
 using namespace std;
 
-GBufferedImage* img;
-double const radius = 400;
-int const MAX_DEPTH = 500;
-double const GW_WIDTH = radius * 2;
-double const GW_HEIGHT = radius * 2;
-double const WIDTH_KOEF = 4/GW_WIDTH;
-double const HEIGHT_KOEF = 4/GW_HEIGHT;
 
+/* Declarations
+ * -----------------------------------------------------------------------------------------*/
+GBufferedImage* img;
+double const RADIUS = 300;              /* Represents radius value "2" on the complex area  */
+int const MAX_DEPTH = 500;              /* Limit for iterations quantity  */
+
+double const GW_WIDTH =  1.3 * RADIUS;  /* Main image parameters, koeficients make          */
+double const GW_HEIGHT = 1.2 * RADIUS;  /* cardioide position being closer to window centre */
+
+/*------------------------------------------------------------------------------------------//
+ * Implementation section.
+ * -----------------------
+ * For current radius parameter program draws
+ * Mandelbrot set image. It main part, cardioide,
+ * is black area in the centre of the window.
+ * Space of program window represent  complex space,
+ * where:
+ * - window top is "-1" imaginary complex value,
+ * - window bottom is "1" imaginary value,
+ * - window left edge is "-2" of real complex value.
+ * User can enter RADIUS value in declarations part
+ * of code to change sizes of program image.
+ * -----------------------------------------------------------------------------------------*/
+
+/* Function: equationProcess
+ * ---------------------------
+ * Returns iterations quantity for current c-point(params a, b),
+ * due to previous Z-value (params zR, zI).
+ *
+ * @a, b       Current complex area c-point: c = a + jb
+ * @zR, zI     Previous complex Z-value parameters: Z = zR + jzI
+ * @iter       Counter of iterations have been made yet  */
 int equationProcess(double a,
                     double b,
                     double zR,
                     double zI,
                     int iter){
+    /* Main Z-value rised to power:
+     * zLenght = Z^2 = zR^2 + jzI^2 */
     double zLength;
+    /* Euaquations for zLength */
     double buf = (zR * zR) - (zI * zI);
     zI = (2 * zR * zI) + b;
     zR = buf + a;
     zLength = (zR * zR) + (zI * zI);
     iter++;
 
+    /* Implementation notes
+     * --------------------------------------------------------------------
+     * In this program  we make such Mandelbrot apreciation:
+     *   if (Z^2 < 4) for every iteration - this c-point belongs
+     *   to Mandelbrot set. If we limit iterations quantity by MAX_DEPTH,
+     *   and if for every iteration this apreciation will be true
+     *   we can finish sequences for this c-point. This c-point is from
+     *   Mandelbrot set then - it will be black on the picture.
+     *
+     *   Else, if we don't meet confition - so we find external point
+     *   for Mandelbrot set, and we could choose color for it due
+     *   to depth iterations value.
+     * -------------------------------------------------------------------- */
+
+    /* Main apreciation process */
     if((zLength < 4) && (iter < MAX_DEPTH)){
-        return equationProcess(a, b, zR, zI, iter);
+        return equationProcess(a, b, zR, zI, iter);//Continue calculations
     }else{
-        return iter;
+        return iter;                                //Stop calculations
     }
 }
 
+/* Function: getIterationDepth
+ * ---------------------------
+ * Returns iterations quantity for current pixel.
+ * Position of Current pixel is modified into
+ * complex area scale position. In this complex area we made
+ * evaluations, to what set this point belongs. If depth value
+ * rises  MAX_DEPTH - so it's internal point of
+ * Manelbrot set, and it will be black on image. If we obtain
+ * smaller value then MAX_DEPTH - determine that this is external point
+ * and we can paint it in some other color.
+ *
+ * @col, row       Current image position  */
 int getIterationDepth(double col, double row){
-    /* Current complex area point (a, b) */
-    double a = WIDTH_KOEF * col - 2;
-    double b = 2 - HEIGHT_KOEF * row;
-    double zR = 0;
-    double zI = 0;
-    int iter = 0;
 
-    int depth = equationProcess(a, b, zR, zI, iter);
+    /* Implementation notes
+     * --------------------------------------------------------------------
+     * The user radius represents R = 2 radius on the
+     * complex area. The main cardioide of the Madelbrot
+     * set is inserted in this circle. In this program
+     * window image position is taken due to radius value,
+     * so the image size depends of the radius and
+     * always represents the same part of the complex circle.
+     * This circle positioned in such way:
+     *  - At the left side of the program image is the
+     *    "-2" real complex value - left edge of the
+     *    Mandelbrot set.
+     *  - Top edge - is "-1" complex imaginary value
+     *  - Image bottom edge - is "1" complex imaginary value
+     *  - Image right side - is > "1" complex real value
+     *  - The real value axis (b = 0) is on the middle of the
+     *    image height.
+     * So, modification of image position is strict, and
+     * requires significant corrections in demand.
+     * --------------------------------------------------------------------*/
+
+    /* Modifies image pixel position into
+     * complex area position (c = a + jb) due
+     * to implementation describtion above   */
+    double a = col *(2 / RADIUS) - 2;   /* a - real complex value */
+    double b = row *(2 / RADIUS) - 1;   /* b - imaginary complex value */
+
+    /* Implementation notes
+     * --------------------------------------------------------------------
+     * The main apreciation parameter of program is Z value,
+     * which forms sequences like this:
+     * Z0 = 0
+     * Znext = Zpreviuos^2 + c
+     * ...
+     *
+     * The main Mandelbrot idea is "|Z| < R" for every iterations depth,
+     * where R could be taken as "2" for simplification.
+     *
+     * We try to discover if this c-point satisfy this condition
+     * for any iteration. If it's true - it will be black point on the image.
+     * --------------------------------------------------------------------*/
+
+    /* Apreciation starts from Z0 = z0R + jz0I = 0.             */
+    double z0R = 0;      /* Represents real value for complex Z0       */
+    double z0I = 0;      /* Represents imaginary value for complex Z0  */
+    int iter = 0;        /* Start value for iterations counter */
+
+    /* Makes sequence of Z-value calculations for current c-point
+     * to find out iterations quantity  */
+    int depth = equationProcess(a, b, z0R, z0I, iter);
 
     return depth;
 }
@@ -51,13 +153,22 @@ int getIterationDepth(double col, double row){
 int main() {
     GWindow gw;
     gw.setSize(GW_WIDTH, GW_HEIGHT);
-    img = new GBufferedImage(GW_WIDTH,GW_HEIGHT,0x00ffff);
+    img = new GBufferedImage(GW_WIDTH, GW_HEIGHT,0xffffff);
     gw.add(img, 0, 0);
+    /* For every image pixel:
+     * - modifies it into complex area scale;
+     * - appreciates, if it satisfy Mandelbrot set condition
+     * - choses color of pixel due to Mandelbrot set condition */
     for(int row = 0; row < GW_HEIGHT; row++){
         for(int col = 0; col < GW_WIDTH; col++){
+            /* Determines main Mandelbrot condition (described above)
+             * for this pixel */
             int currentDepth = getIterationDepth(col, row);
-            int color = 255*(MAX_DEPTH - currentDepth)/MAX_DEPTH;
-            int pixelColor = color * color *color;
+            /* Set color for pixel:
+             * - if currentDepth = MAX_DEPTH - it's black pixel
+             * - if currentDepth < MAX_DEPTH - set apropriate color */
+            int color = 255 *(MAX_DEPTH - currentDepth)/MAX_DEPTH;
+            int pixelColor = color * color * color;
             img->setRGB(col, row, pixelColor);
         }
     }
--- Sierpinski/src/Sierpinski.cpp
+++ Sierpinski/src/Sierpinski.cpp
@@ -1,30 +1,146 @@
-﻿#include <iostream>
-#include "console.h"
-#include <stdlib.h>
-#include "queue.h"
-#include <Vector>
-#include "gbufferedimage.h"
+﻿/********************************************************************************************
+ * File: Sierpinski.cpp
+ * ----------------------
+ * v.1 2015/11/30
+ * Implementation of the Sierpinski triangle drawing.
+ ********************************************************************************************/
 
+#include <iostream>
+#include "console.h"
+#include "simpio.h"
+#include "gbufferedimage.h"
 
 using namespace std;
 
-GBufferedImage* img;
 
-void printPoint(GPoint& pt){
-    cout << pt.getX() << " " << pt.getY() << endl;
+/* Declarations
+ * -----------------------------------------------------------------------------------------*/
+GBufferedImage* img;                /* Main program image */
+double const MAX_LENGTH = 1000;     /* Max side length value for 0-oreder triangle */
+double const MIN_SIDE = 2;          /* Minimal inserted triangle side to make nice picture */
+double const MIN_WINDOW = 100;      /* Minimal size of main window */
+
+/* -----------------------------------------------------------------------------------------//
+ * Implementation section.
+ * -----------------------
+ * Program asks user to enter length
+ * of the bigest, 0-order, triangle, and then asks
+ * to enter desireable order for Sierpinski triangle
+ * drawing.
+ * Then it recursively draws Sierpinski triangle.
+ * -----------------------------------------------------------------------------------------*/
+
+/* Function: minimalOrder
+ * ----------------------
+ * Returns maximal order value, for param length, due
+ * to minLine value.
+ * MinLine value - is minimal side length for internal
+ * triangle, which could be obtained due to recursive
+ * 0-order triangle dividing.
+ *
+ * @min           Minimal valid triangle side length.
+ * @lenght        User input 0-oreder triangle length.
+ * @currentOrder  Rcusion current order   */
+int maxOrder(double minLine, double length, int currentOrder){
+    if (length <= minLine){
+        return currentOrder;
+    }else{
+        return maxOrder(minLine, (length/2), ++currentOrder);
+    }
 }
 
+/* Function: userInput
+ * -------------------
+ * Asks user to enter length
+ * of the bigest, 0-order triangle, and then asks
+ * to enter desireable order for Sierpinski triangle
+ * drawing.
+ * Initiates values of params - lenght, inputOrder.
+ * Makes prompts for optimal user input.
+ *
+ * @lenght        User input 0-oreder triangle length.
+ * @inputOrder    User desirable Sierpinski order   */
+void userInput(double& length, int& inputOrder){
+    /* Asks for 0-oreder triangle length */
+    while(true){
+        length = (double)getInteger("ENTER 0-ORDER TRIANGLE SIDE LENGTH: ");
+        if ((length > 0) && (length < MAX_LENGTH)){ break;}
+        else{cout << "      VALID LENGTH RANGE SHOULD BE [0 - " << MAX_LENGTH
+                  << "]" << endl;}
+    }
+
+    /* Choses optimal order due to input length.
+     * Such order makes triangle picture more clear, and
+     * dosen't rise recursion level to much */
+    int optimalOrder = maxOrder(MIN_SIDE, length, 0);
+
+    /* Asks for desirable Sierpinski order */
+    while(true){
+        inputOrder = (double)getInteger("ENTER SIERPINSKI ORDER: ");
+        /* Asks user to make choose due to optimal conditions */
+        if ((inputOrder > 0) && (inputOrder <= optimalOrder)){ break;}
+        else if(inputOrder > optimalOrder){cout << "      OPTIMAL ORDER IS LOWER THEN: "
+                                                << optimalOrder << endl;}
+        else if(inputOrder < 0){cout << "      NOT VALID ORDER" << endl;}
+    }
+}
+
+/* Function: setTriangleTopInWindow
+ * --------------------------------
+ * Choose 0-oreder triangle position in the
+ * main window. Sets window sizes due to
+ * triangle size.
+ * Initiates triangle top vertex position
+ * params - vertex_X, vertex_Y.
+ *
+ * @gw          Main program window object.
+ * @lenght      User input 0-oreder triangle length.
+ * @vertex_X    0-oreder triangle top vertex position
+ * @vertex_Y                                         */
+void setTriangleTopInWindow(GWindow& gw,
+                            double length,
+                            double& vertex_X,
+                            double& vertex_Y){
+    double triangleHeight = length * 0.866;
+    double gwWidth = length + (0.2 * length);
+    double gwHeight = triangleHeight +(0.3 * triangleHeight);
+
+    if(gwHeight < MIN_WINDOW){
+        gw.setSize(MIN_WINDOW, MIN_WINDOW);
+    }else{
+        gw.setSize(gwWidth, gwHeight);
+    }
+
+    vertex_X = gwWidth / 2;
+    vertex_Y = 0.1 * triangleHeight;
+}
+
+/* Function: drawTriangle
+ * ----------------------
+ * Draws main big triangle of the image.
+ * Start of drawing begins from topPt.
+ * Initiates positions of triangle vertexes into
+ * params - rightPt and leftPt.
+ *
+ * @gw          Main program window object.
+ * @topPt       Top vertex - start of drawing
+ * @rightPt     righ vertex - value is set in function
+ * @leftPt      left vertex - value is set in function
+ * @length      Triangle side length.        */
 void drawTriangle(GWindow& gw,
-                  GPoint& startPt,
-                  GPoint& rightPt,
-                  GPoint& leftPt,
                   GPoint& topPt,
+                  GPoint& rightPt,
+                  GPoint& leftPt,                  
                   int length){
-    rightPt = gw.drawPolarLine(startPt, length, -60);
+    rightPt = gw.drawPolarLine(topPt, length, -60);
     leftPt = gw.drawPolarLine(rightPt, length, 180);
-    topPt = gw.drawPolarLine(leftPt, length, 60);
+    gw.drawPolarLine(leftPt, length, 60);
 }
 
+/* Function: getMiddle
+ * ----------------------
+ * Returns point which is positioned
+ * on the middle, between param points. */
 GPoint getMiddle(GPoint& pt1, GPoint& pt2){
     double x1 = pt1.getX();
     double y1 = pt1.getY();
@@ -36,12 +152,28 @@ GPoint getMiddle(GPoint& pt1, GPoint& pt2){
     return pt;
 }
 
+/* Function: drawSmallSide
+ * -----------------------
+ * Draws line between param points.
+ * This line is side of the inserted triangle.  */
 void drawSmallSide(GWindow& gw,
                   GPoint& pt1,
                   GPoint& pt2){
     gw.drawLine(pt1.getX(), pt1.getY(), pt2.getX(), pt2.getY());
 }
 
+/* Function: triangleProcess
+ * -------------------------
+ * Main program recursion. Insert three small
+ * triangles inside external biger triangle, while
+ * current oreder isn't smaller then input order.
+ *
+ * @gw              Main program window object.
+ * @currentOrder    Order of triangles which are inserted yet.
+ * @inputOrder      User order.
+ * @topPt           Top, righ, left edge vertexes
+ * @rightPt         of external bigger triangle.
+ * @leftPt                                                   */
 void triangleProcess(GWindow& gw,
                       int currentOrder,
                       int inputOrder,
@@ -49,31 +181,33 @@ void triangleProcess(GWindow& gw,
                       GPoint& rightPt,
                       GPoint& leftPt){
     if(currentOrder <= inputOrder){
+        /* Determines middle points of external triangle sides */
         GPoint rightSideMiddle = getMiddle(rightPt, topPt);
         GPoint bottomSideMiddle = getMiddle(rightPt, leftPt);
         GPoint leftSideMiddle = getMiddle(leftPt, topPt);
-        /* Top  shelf */
+        /* Draws top shelf - side of the top inserted triangle */
         drawSmallSide(gw, leftSideMiddle, rightSideMiddle);
-        /* Right shelf */
+        /* Draws right shelf - side of the right inserted triangle */
         drawSmallSide(gw, rightSideMiddle, bottomSideMiddle);
-        /* Left  shelf */
+        /* Draws left shelf - side of the left inserted triangle */
         drawSmallSide(gw, leftSideMiddle, bottomSideMiddle);
+        /* Update current order for current triangle */
         currentOrder++;
-        /* Call top triangle pocess */
+        /* Recursively calls top triangle pocess */
         triangleProcess(gw,
                         currentOrder,
                         inputOrder,
                         topPt,
                         rightSideMiddle,
                         leftSideMiddle);
-        /* Call right triangle pocess */
+        /* Recursively calls right triangle pocess */
         triangleProcess(gw,
                         currentOrder,
                         inputOrder,
                         rightSideMiddle,
                         rightPt,
                         bottomSideMiddle);
-        /* Call left triangle pocess */
+        /* Recursively calls left triangle pocess */
         triangleProcess(gw,
                         currentOrder,
                         inputOrder,
@@ -82,42 +216,36 @@ void triangleProcess(GWindow& gw,
                         leftPt);
     }
 }
-/*
- * - разобраться с высотой окна
- * - оформить ввод и вывод
- * - комментарии
- * - минимально возможный порядок прикинуть
- * - коммит
- *
-*/
 
 int main() {
     GWindow gw;
-    double length = 300;
-    int inputOrder = 6;
-    double triangleHeight = length * 0.866;
-    double gwWidth = length + (0.2 * length);
-    double gwHeight = triangleHeight +(0.2 * triangleHeight);
-    double vertex_X = gwWidth / 2;
-    double vertex_Y = 0.1 * triangleHeight;
 
-    gw.setSize(gwWidth, gwHeight);
+    double sideLength;      /* 0-order triangle side length */
+    int inputOrder;         /* User order request */
+    /* Asks user to enter length and order */
+    userInput(sideLength, inputOrder);
 
-    GPoint top(vertex_X, vertex_Y);
-    GPoint topPt;
-    GPoint rightPt;
-    GPoint leftPt;
-    int currentOrder = 0;
-        drawTriangle(gw, top, rightPt, leftPt, topPt, length);
-        currentOrder++;
-        /* Start recursion */
-        triangleProcess(gw,
-                        currentOrder,
-                        inputOrder,
-                        topPt,
-                        rightPt,
-                        leftPt);
-        /* Recursion stopped */
+    /* Finds position of the top vertex of 0-order triangle */
+    double vertex_X;
+    double vertex_Y;
+    /* Adjusts window and set triangle top vertex position */
+    setTriangleTopInWindow(gw, sideLength, vertex_X, vertex_Y);
+
+    GPoint topPt(vertex_X, vertex_Y);/* Initiate top vertex */
+    GPoint rightPt;                  /* Right vertex */
+    GPoint leftPt;                   /* Left vertex */
+    int currentOrder = 0;            /* Current drawing order */
+
+    /* Draws 0-order triangle and initiate rightPt and leftPt  */
+    drawTriangle(gw, topPt, rightPt, leftPt, sideLength);
+    currentOrder++;
+    /* Starts    recursion */
+    triangleProcess(gw,
+                    currentOrder,
+                    inputOrder,
+                    topPt,
+                    rightPt,
+                    leftPt);
 
    return 0;
 }
